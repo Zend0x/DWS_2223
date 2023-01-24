9cdc7ee50af16f2d5746d63d9f8877bec07e21d4
fernando_buendia <fbuendia@protonmail.com>
2023-01-24 20:44:33 +0100

    24Enero-SQL Renovado

--- U4/practica/torneos/sql/db.sql
+++ U4/practica/torneos/sql/db.sql
@@ -18,17 +18,29 @@ CREATE TABLE T_usuarios(
 CREATE TABLE T_torneos(
 	id_torneo int(5) auto_increment primary key,
     nombre varchar(100) NOT NULL,
+    fecha DATE NOT NULL,
     localizacion varchar(50)
 );
 
 CREATE TABLE T_partidos(
 	id_partido int(6) auto_increment primary key,
     id_torneo int(5),
-    idJugadorA int(6),
-    idJugadorB int(6),
-    idGanador int(6),
+    id_jugadorA int(6),
+    id_jugadorB int(6),
+    id_ganador int(6),
     FOREIGN KEY (id_torneo) REFERENCES T_torneos (id_torneo),
-    FOREIGN KEY (idJugadorA) REFERENCES T_jugadores(id_jugador),
-    FOREIGN KEY (idJugadorB) REFERENCES T_jugadores(id_jugador),
-    FOREIGN KEY (idGanador) REFERENCES T_jugadores(id_jugador)
+    FOREIGN KEY (id_jugadorA) REFERENCES T_jugadores(id_jugador),
+    FOREIGN KEY (id_jugadorB) REFERENCES T_jugadores(id_jugador),
+    FOREIGN KEY (id_ganador) REFERENCES T_jugadores(id_jugador)
 );
+
+insert into T_torneos(nombre,localizacion,fecha) VALUES ('Gran Torneo de la Copa Estrella del Universo','Mushroom Kingdom','2022/12/22');
+insert into T_torneos(nombre,localizacion,fecha) VALUES ('Gran Torneo Interestelar de la Copa Pistón de Doc Hudson by Rust Eze','Radiador Springs','2021/06/20');
+
+INSERT INTO T_jugadores(nombre,apellidos,nacionalidad) VALUES ('Jaime','Águilas','UZ');
+INSERT INTO T_jugadores(nombre,apellidos,nacionalidad) VALUES ('Charles', 'White', 'USA');
+
+INSERT INTO T_partidos(id_torneo,id_jugadorA,id_jugadorB,id_ganador) VALUES (1,1,2,2);
+
+SELECT * FROM T_partidos;
+SELECT * from T_torneos;
